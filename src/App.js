c2174aa008bd96acede0103cf0c29c73e4562f92
AndreenkovAndrey <151832160+AndreenkovAndrey@users.noreply.github.com>
2024-05-05 17:21:35 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React, { useEffect, useState } from "react";
-import TrackedCityList from "./TrackedCityList";
+
 import SearchCityData from "./SearchCityData";
 
-import getCurrentWeatherAndForecastResponse from "./WeatherForecastAPI";
-import ConvertForecastData from "./ConvertForecastData";
-import ForecastDataUpload from "./ForecastDataUpload";
 
 export const App = () => {
   const [citySearchQuery, setСitySearchQuery] = useState("");
@@ -13,42 +10,18 @@ export const App = () => {
 
   const { cityListForDropdown, getCityInfo } = SearchCityData();
 
-  // openmeteo api try
-
-  const { currentWeather, getCurrentWeather } = ConvertForecastData();
-
-  async function loadWeatherData(city) {
-    const data = await getCurrentWeatherAndForecastResponse(city);
-    console.log(data);
-
-    if (data) {
-      getCurrentWeather(data);
-      // console.log(currentWeather);
-    } else {
-      console.log("Error here");
-    }
-  }
-
-  // useEffect(() => {
-  //   if (selectedCity) loadWeatherData(selectedCity);
-  // }, [selectedCity]);
-
-  const { trackedCity, addTrackedCity, deleteTrackedCity } = TrackedCityList();
 
   const handleClickCity = (city) => {
     const { name, latitude, longitude } = city;
     setSelectedCity({ name, latitude, longitude });
-    loadWeatherData(selectedCity);
   };
 
   const handleChangeInput = (e) => {
     getCityInfo(citySearchQuery);
     setСitySearchQuery(e.target.value);
-    // getCityInfo(e.target.value);
   };
 
   const handleClickOnInputCityList = (city) => {
-    addTrackedCity(city);
     setСitySearchQuery("");
   };
 
@@ -78,23 +51,6 @@ export const App = () => {
       )}
       <p>Избранные города:</p>
       {/* список отслеживаемых городов */}
-      <ul className="trackedCityList">
-        {trackedCity.map((city) => (
-          <li
-            className="liElementOfTrackedCityList"
-            key={city.name}
-            onClick={(e) => handleClickCity(city)}
-          >
-            {city.name}
-            <input
-              type="button"
-              value="Х"
-              className="deletedButtonOfTrackedCityList"
-              onClick={(e) => deleteTrackedCity(city.name)}
-            />
-          </li>
-        ))}
-      </ul>
     </div>
   );
 };
